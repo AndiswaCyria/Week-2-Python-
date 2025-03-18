e95932044425294be306c2020f5b808b82349682
AndiswaCyria <andiswacyriam@gmail.com>
2025-03-18 14:21:17 +0200

    sets

--- Week 2/sets.py
+++ Week 2/sets.py
@@ -1,4 +1,5 @@
 student_id = {2118977, 2118978, 2118979, 2118980, 2118981}
+student_id.add(2118982)
 student_name = {"John", "Jane", "Doe", "Smith", "Alex"}
 student_age = {25, 26, 27, 28, 29}
 student_marks = {70, 80, 90, 100, 60}
@@ -10,3 +11,4 @@ print('Student Name:', student_name)
 print('Student Age:', student_age)
 print('Student Marks:', student_marks)
 print('Student Grade:', student_grade)
+
