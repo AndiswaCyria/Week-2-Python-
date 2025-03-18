fde34105b1cebef1b8852b764b3e322dde1ff4ee
AndiswaCyria <andiswacyriam@gmail.com>
2025-03-18 14:14:13 +0200

    Sets have entered the chat

---
+++ Week 2/sets.py
@@ -0,0 +1,12 @@
+student_id = {2118977, 2118978, 2118979, 2118980, 2118981}
+student_name = {"John", "Jane", "Doe", "Smith", "Alex"}
+student_age = {25, 26, 27, 28, 29}
+student_marks = {70, 80, 90, 100, 60}
+student_grade = {"A", "B", "C", "D",
+                 "E"}  # A is the highest grade and E is the lowest grade
+
+print('Student ID:', student_id)
+print('Student Name:', student_name)
+print('Student Age:', student_age)
+print('Student Marks:', student_marks)
+print('Student Grade:', student_grade)
